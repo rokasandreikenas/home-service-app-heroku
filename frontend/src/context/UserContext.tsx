7dcb85fb050c02c0e45eb6f3df15ed415dd17c81
rokasandreikenas <shyptt@gmail.com>
2024-06-17 21:15:00 +0300

    Initial commit

---
+++ frontend/src/context/UserContext.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren, createContext } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import { LoginResponse, User } from "@/components/user/types";
+
+const UserContext = createContext<{
+  user: User | null;
+  isLoggedIn: boolean;
+  login: (user: LoginResponse) => void;
+  logout: () => void;
+}>({
+  user: null,
+  isLoggedIn: false,
+  login: () => {},
+  logout: () => {},
+});
+
+const UserProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useLocalStorage<User | null>("user", null);
+  const [, setToken] = useLocalStorage<string | null>("token", null);
+
+  const isLoggedIn = !!user;
+
+  const login = (loginResponse: LoginResponse) => {
+    setUser(loginResponse.user);
+    setToken(loginResponse.token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setToken(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, isLoggedIn, login, logout }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserProvider, UserContext };
